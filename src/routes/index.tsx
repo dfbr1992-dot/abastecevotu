40dc8601c62f483c45013bc0d83bd3f5e3edf644
Douglas Ramos <dfbr1992-dot@users.noreply.github.com>
2026-06-02 10:02:17 -0300

    debug: adiciona log para descobrir nome da coluna

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -464,7 +464,7 @@ function Index() {
       }`}
     >
          <h3 className={`text-lg font-black uppercase mb-1 ${isDark ? "text-white" : "text-zinc-900"}`}>
-  {s.company || s.companyName || s.company_name || "Nome da Empresa"}
+  {<s className="nome">{s.nome} </s>|| "Serviço sem nome"}
 </h3>
 
       <p className={`text-sm font-bold mb-3 ${isDark ? "text-blue-400" : "text-blue-600"}`}>
